744c5346fe02a30454c14b6bea36377727d34bdd
reza <rezaf7326@gmail.com>
2024-11-18 23:59:14 +0000

    feat: add message api

---
+++ src/api/message/controllers/message.ts
@@ -0,0 +1,43 @@
+/**
+ * message controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController("api::message.message", {
+  async find(ctx) {
+    strapi.log.debug("find messages");
+    const user = ctx.state.user;
+    ctx.query.filters = {
+      ...((ctx.query.filters || {}) as { [key: string]: unknown }),
+      $or: [{ sender: { id: user.id } }, { receiver: { id: user.id } }],
+    };
+    strapi.log.debug(`modified filters: ${JSON.stringify(ctx.query.filters)}`);
+
+    return super.find(ctx);
+  },
+
+  async delete(ctx) {
+    strapi.log.debug("extending delete message");
+    const user = ctx.state.user;
+    const msgId = ctx.params.id;
+    strapi.log.debug(
+      `delete requets: ${JSON.stringify({ user: user.id, msgId })}`
+    );
+    if (!msgId) {
+      return ctx.badRequest("Message id is required.");
+    }
+    const msg = await strapi
+      .service("api::message.message")
+      .findOne(msgId, { populate: ["sender"] });
+    strapi.log.debug(`msg to delete: ${JSON.stringify(msg)}`);
+    if (!msg || msg.sender.id !== user.id) {
+      return ctx.forbidden("You are not authorized to delete this message.");
+    }
+    const deletedMsg = await strapi
+      .service("api::message.message")
+      .delete(msgId);
+
+    return deletedMsg;
+  },
+});
